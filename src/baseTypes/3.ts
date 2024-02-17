9e5c47b8546ad3b6bff7beed2c342876b72b2fb5
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2024-02-17 13:15:01 +0100

    goit-typescript-hw-02-commit 2

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,11 @@
-let some:unknown;
+let some: unknown;
 some = 'Text';
 let str: string;
-str = some;
+// str = some;   ----- безпосереднє присвоєння значення змінній як іншу змінну небезпечне, 
+// тож без перевірки TS не пропустить, тому потрібна перевірка типу через typeof
+if ( typeof some === "string") {
+    str = some;
+}
+
 
 export {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,25 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekend {
+
+  sunday = "Sunday",
+monday = "Monday",
+tuesday = "Tuesday",
+wednesday = "Wednesday",
+thursday = 'Thursday',
+friday = "Friday",
+saturday = 'Saturday'
+ }; 
+
+ function isWeekend (day: Weekend) :boolean {
+  if (day === Weekend.saturday || day === Weekend.sunday) {
+    return true;
+  }
+return false;
+ }
+
+ console.log(isWeekend(Weekend.friday))
+ console.log(isWeekend(Weekend.saturday))
+
